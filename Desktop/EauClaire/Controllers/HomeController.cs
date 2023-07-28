4c3d14085a8fcd52d7e73bd416e3624baba0c900
Natalie Benjes <natalie.benjes@gmail.com>
2023-07-28 10:04:48 -0700

    WIP DB context file

--- Desktop/EauClaire/Controllers/HomeController.cs
+++ Desktop/EauClaire/Controllers/HomeController.cs
@@ -1,31 +1,17 @@
-﻿using System.Diagnostics;
+﻿
 using Microsoft.AspNetCore.Mvc;
-using EauClaire.Models;
 
-namespace EauClaire.Controllers;
-
-public class HomeController : Controller
+namespace BestRestaurantsApp.Controllers
 {
-    private readonly ILogger<HomeController> _logger;
 
-    public HomeController(ILogger<HomeController> logger)
+  public class HomeController : Controller
+  {
+
+    [HttpGet("/")]
+    public ActionResult Index()
     {
-        _logger = logger;
+      return View();
     }
 
-    public IActionResult Index()
-    {
-        return View();
-    }
-
-    public IActionResult Privacy()
-    {
-        return View();
-    }
-
-    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-    public IActionResult Error()
-    {
-        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
-    }
+  }
 }
