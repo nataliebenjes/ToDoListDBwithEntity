d4e3cca27bdf73041e8d2547c54179f25458bf29
Natalie Benjes <natalie.benjes@gmail.com>
2023-07-30 16:51:59 -0700

    fix typo in stylistsController

--- Desktop/HairSalonApp/HairSalon.Solution/Controllers/StylistsController.cs
+++ Desktop/HairSalonApp/HairSalon.Solution/Controllers/StylistsController.cs
@@ -6,11 +6,11 @@ using System.Linq;
 
 namespace HairSalon.Controllers
 {
-  public class Stylists : Controller
+  public class StylistsController : Controller
   {
     private readonly HairSalonContext _db;
 
-    public StylistController(HairSalonContext db)
+    public StylistsController(HairSalonContext db)
     {
       _db = db;
     }
